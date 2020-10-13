02815877d4e2078e8cce443ee07cdd0daa2b8de3
Amit-Pallauri-au6 <apallauri@gmail.com>
2020-10-13 18:09:04 +0530

    fuzzy search tweaks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Loading from '../components/Loading'
 import '../styles/homepage.css'
 import { Card, CardBody, CardImg, CardTitle } from 'reactstrap'
-import FuzzySearch from 'fuzzy-search'
+import Fuse from 'fuse.js'
 import Axios from 'axios'
 import { Modal } from 'antd';
 import { Link } from 'react-router-dom'
@@ -11,6 +11,7 @@ import { UserOutlined, UpCircleOutlined, FileImageOutlined } from '@ant-design/i
 const HomePage = () => {
 
     const[files, setData] = useState('')
+    const[searchedFiles, setSearchFiles] = useState('')
     const[searchQuery, setQuery] = useState('')
     const[visible, setVisible] = useState(false);
     const[imageData, setFile] = useState('')
@@ -21,49 +22,73 @@ const HomePage = () => {
         }).catch(() => setData(' '))
     },[])
 
-    const searcher = new FuzzySearch( files, ['data.title'])
+    let fuse = ""
+
+    if(files){
+        fuse = new Fuse( files, {
+            keys : [
+                'data.title'
+            ]
+        })
+    }
+    
+    const results = fuse.search(searchQuery)
+    
     useEffect(()=>{
-        const results = searcher.search(searchQuery)
-        setData(results)
+        setSearchFiles(results)
     }, [searchQuery])
     
-
+    console.log(searchedFiles)
 
     const handleClick = (index) => {
-        setFile(files[index].data)
+        if(searchedFiles.length !==0 && searchedFiles !== 0){
+            setFile(searchedFiles[index].item.data)
+        }else if(files){
+            setFile(files[index].data)
+        }
         setVisible(true)
+    }
+
+    var elem = ''
+    var posts = ''
+    
+    if(searchedFiles.length !==0 && searchedFiles !== 0){
+        posts = searchedFiles
+    }else if( files ){
+        posts = files
     }
 
     return (
         <>
             <div className='homepage-container'>
                 {
-                    files ?
+                    posts ?
                     <>
                         <div className='search-container'>
                             <input type="text" placeholder='search it up' value={searchQuery} onChange={ e => setQuery(e.target.value) }/>
                         </div>
                         <div className='row' style={{ width : '95%', margin : 'auto' }}>
                             {
-                                files.map( (el, index) =>{
+                                posts.map( (el, index) =>{
+                                    elem = el.item ? el.item : el
                                     return(
                                         <div key={index} className="col-lg-3 col-md-6 col-sm-12">
                                             <Link to='/' onClick={() => handleClick(index)}>
                                                 <Card style={{ minHeight : '22vw' }}>
-                                                    <CardImg top width="100%" style={{ padding : '20px', height: '15vw', objectFit : 'cover'}} src={el.data.thumbnail} />
+                                                    <CardImg top width="100%" style={{ padding : '20px', height: '15vw', objectFit : 'cover'}} src={elem.data.thumbnail} />
                                                     <CardBody>
                                                         <CardTitle style={{ 
                                                             textAlign : "center",
                                                             fontWeight: 700,
                                                             fontSize: '18px',
                                                             fontFamily: 'Caveat, cursive'
-                                                        }}>{el.data.title}</CardTitle>
+                                                        }}>{elem.data.title}</CardTitle>
                                                     </CardBody>
                                                 </Card>
                                             </Link>
                                         </div> 
-                                    )})     
-                            }                     
+                                )})  
+                            }                 
                         </div>
                     </>
                     :
